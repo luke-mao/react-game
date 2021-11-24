2d7a1b82948b6d02c8d7825d53b8b397e199093c
luke mao <jinjie.luke.mao@gmail.com>
2021-11-24 17:19:53 +1100

    finish sidebar

---
+++ src/components/Sidebar.js
@@ -0,0 +1,123 @@
+import React from 'react'
+import { Box } from '@mui/system';
+import Logo from '../images/logo.png';
+import { Avatar, Link, Stack, Typography } from '@mui/material';
+import { Link as RouterLink } from "react-router-dom";
+import { useTheme } from '@mui/material/styles';
+
+const styles = (theme) => ({
+  sidebar: {
+    position: 'fixed',
+    backgroundColor: '#eee', 
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    height: '100vh',
+    width: '100px',
+
+    // the width changes between 30px, 60px, 100px
+    [theme.breakpoints.down('md')]: {
+      width: '30px',
+    },
+    [theme.breakpoints.only('md')]: {
+      width: '60px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: '100px',
+    },
+  },
+  logo: {
+    marginTop: '15px',
+    marginBottom: '15px',
+    height: '50px',
+    width: '50px',
+
+    // logo disappears for under 800px
+    display: {
+      xs: 'none',
+      sm: 'none',
+      md: 'block',
+      lg: 'block',
+      xl: 'block',
+    },
+  },
+  stackLinks: {
+    mt: 4,
+  },
+  // sepaaration of 1400px, the textcontent changes
+  displaySmall: {
+    // below 800px
+    display: {
+      xs: 'block',
+      sm: 'block',
+      md: 'block',
+      lg: 'none',
+      xl: 'none'
+    }
+  },
+  displayBig: {
+    // more than 1400px
+    display: {
+      xs: 'none',
+      sm: 'none',
+      md: 'none',
+      lg: 'block',
+      xl: 'block',
+    }
+  }
+});
+
+export default function Sidebar() {
+  console.log(styles);
+  const theme = useTheme();
+
+  return (
+    <Box
+      sx={styles(theme).sidebar}
+    >
+      <Avatar
+        variant='square'
+        sx={styles(theme).logo}
+        src={Logo}
+        alt='logo'
+      />
+      <Stack
+        sx={styles(theme).stackLinks}
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={6}
+      >
+        {/* each link has three display mode */}
+        <Link 
+          to='/home'
+          component={RouterLink}
+        >
+          <Typography sx={styles(theme).displaySmall}>H</Typography>
+          <Typography sx={styles(theme).displayBig}>Home</Typography>
+        </Link>
+        <Link 
+          to='/tictac'
+          component={RouterLink}
+        >
+          <Typography sx={styles(theme).displaySmall}>Ti</Typography>
+          <Typography sx={styles(theme).displayBig}>Tictac</Typography>
+        </Link>
+        <Link 
+          to='/tower'
+          component={RouterLink}
+        >
+          <Typography sx={styles(theme).displaySmall}>To</Typography>
+          <Typography sx={styles(theme).displayBig}>Tower</Typography>
+        </Link>
+        <Link 
+          to='/snek'
+          component={RouterLink}
+        >
+          <Typography sx={styles(theme).displaySmall}>S</Typography>
+          <Typography sx={styles(theme).displayBig}>Snek</Typography>
+        </Link>
+      </Stack>
+    </Box>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// change the theme to:
+// md: 800px, lg: 1400px, and others keep the same
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 800,
+      lg: 1400,
+      xl: 1536,
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider
+      theme={theme}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
